7abf8d86315d42f5930271c2d3c9340dfcea2445
Gabriel Sandoli <gsaboto@gmail.com>
2020-11-12 16:57:24 -0300

    Revertendo commit errado na master.

--- flutter_iclothes/android/app/src/main/kotlin/com/example/flutter_iclothes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_iclothes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_iclothes/lib/routes/app_routes.dart
+++
@@ -1,4 +0,0 @@
-class AppRoutes {
-  static const HOME = '/';
-  static const ESTABELECIMENTOS_FORMS = '/estabelecimento-forms';
-}

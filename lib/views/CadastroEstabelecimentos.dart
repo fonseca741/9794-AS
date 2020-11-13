f1dae76e0105c43b4b7ca9aef8918b1d57a327d6
Gabriel Sandoli <gsaboto@gmail.com>
2020-11-13 14:13:59 -0300

    Fim do login e ajustes no cadastro de Estabelecimentos

--- lib/views/CadastroEstabelecimentos.dart
+++ lib/views/CadastroEstabelecimentos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:iclothes/models/Estabelecimento.dart';
 import 'package:iclothes/provider/providerEstabelecimentos.dart';
 import 'package:provider/provider.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class EstabelecimentoForms extends StatefulWidget {
   @override
@@ -24,151 +25,188 @@ class _EstabelecimentoFormsState extends State<EstabelecimentoForms> {
     }
   }
 
+  void cadastrar() {
+    if (_form.currentState.validate()) {
+      _form.currentState.save();
+      Provider.of<ProviderEstabelecimentos>(context, listen: false).put(
+        Estabelecimento(
+          id: _formData['id'],
+          nome: _formData['nome'],
+          telefone: _formData['tel'],
+          endereco: _formData['end'],
+          email: _formData['email'],
+          senha: _formData['senha'],
+        ),
+      );
+      Alert(
+        context: context,
+        type: AlertType.success,
+        title: "Bem Vindo!",
+        desc: "Conta criada com sucesso.",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "OK",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+      Navigator.of(context).pop(); //Volta pro Login
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final estab = ModalRoute.of(context).settings.arguments as Estabelecimento;
     loadFormData(estab);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Cadastro de Estabelecimento',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.save,
+        appBar: AppBar(
+          title: Text(
+            'Cadastro de Estabelecimento',
+            style: TextStyle(
               color: Colors.white,
             ),
-            onPressed: () {
-              final isValid = _form.currentState.validate();
-              if (isValid) {
-                _form.currentState.save();
-                Provider.of<ProviderEstabelecimentos>(context, listen: false)
-                    .put(
-                  Estabelecimento(
-                    id: _formData['id'],
-                    nome: _formData['nome'],
-                    telefone: _formData['tel'],
-                    endereco: _formData['end'],
-                    email: _formData['email'],
-                    senha: _formData['senha'],
-                  ),
-                );
-                Navigator.of(context).pop();
-              }
-            },
           ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: _form,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                initialValue: _formData['nome'],
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                    labelText: 'Nome do Estabelecimento',
-                    border: new OutlineInputBorder(
-                        borderRadius:
-                            const BorderRadius.all(const Radius.circular(20)))),
-                validator: (value) {
-                  if (value.trim().isEmpty) {
-                    return 'Insira o nome do Estabelecimento';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData['nome'] = value;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['tel'],
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                    labelText: 'Telefone do Estabelecimento',
-                    border: new OutlineInputBorder(
-                        borderRadius:
-                            const BorderRadius.all(const Radius.circular(20)))),
-                validator: (value) {
-                  if (value.trim().isEmpty) {
-                    return 'Insira o telefone do Estabelecimento';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData['tel'] = value;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['end'],
-                keyboardType: TextInputType.streetAddress,
-                decoration: InputDecoration(
-                    labelText: 'Endereço do Estabelecimento',
-                    border: new OutlineInputBorder(
-                        borderRadius:
-                            const BorderRadius.all(const Radius.circular(20)))),
-                validator: (value) {
-                  if (value.trim().isEmpty) {
-                    return 'Insira o endereço do Estabelecimento';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData['end'] = value;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['email'],
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'Email para login',
-                    border: new OutlineInputBorder(
-                        borderRadius:
-                            const BorderRadius.all(const Radius.circular(20)))),
-                validator: (value) {
-                  if (value.trim().isEmpty) {
-                    return 'Insira um email para login';
-                  } else if (!value.contains('@') || !value.endsWith('.com')) {
-                    return 'Insira um email válido';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData['email'] = value;
-                },
-              ),
-              TextFormField(
-                initialValue: _formData['senha'],
-                decoration: InputDecoration(
-                    labelText: 'Senha para login',
-                    border: new OutlineInputBorder(
-                        borderRadius:
-                            const BorderRadius.all(const Radius.circular(20)))),
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                validator: (value) {
-                  if (value.trim().isEmpty) {
-                    return 'Insira uma senha para login';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _formData['senha'] = value;
-                },
-              ),
-            ],
-          ),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: _form,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    initialValue: _formData['nome'],
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                        labelText: 'Nome do Estabelecimento',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira o nome do Estabelecimento';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData['nome'] = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    initialValue: _formData['tel'],
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        labelText: 'Telefone do Estabelecimento',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira o telefone do Estabelecimento';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData['tel'] = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    initialValue: _formData['end'],
+                    keyboardType: TextInputType.streetAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Endereço do Estabelecimento',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira o endereço do Estabelecimento';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData['end'] = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    initialValue: _formData['email'],
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email para login',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira um email para login';
+                      } else if (!value.contains('@') ||
+                          !value.endsWith('.com')) {
+                        return 'Insira um email válido';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData['email'] = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    initialValue: _formData['senha'],
+                    decoration: InputDecoration(
+                        labelText: 'Senha para login',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira uma senha para login';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _formData['senha'] = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Cadastrar',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    textColor: Colors.white,
+                    color: Color.fromRGBO(95, 140, 245, 1),
+                    padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iclothes/routes/app_routes.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,70 +11,70 @@ class _LoginState extends State<Login> {
   //final Map<String, Object> _formData = {};
 
   void verificarLogin() {
-    if (_form.currentState.validate()) {
-      //Navegar para home
-    }
+    // if (_form.currentState.validate()) {
+    //   Navigator.pushReplacementNamed(context, AppRoutes.HOME);
+    // }
+    Navigator.pushReplacementNamed(context, AppRoutes.HOME);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      body: ListView(
         padding: EdgeInsets.all(16),
-        child: Form(
-          key: _form,
-          child: Column(
-            children: <Widget>[
-              Image(
-                image: AssetImage('lib/assets/foto_login.jpeg'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: 'Digite o Email',
-                      border: new OutlineInputBorder(
-                          borderRadius: const BorderRadius.all(
-                              const Radius.circular(20)))),
-                  validator: (value) {
-                    if (value.trim().isEmpty) {
-                      return 'Insira um email para login';
-                    } else if (!value.contains('@') ||
-                        !value.endsWith('.com')) {
-                      return 'Insira um email válido';
-                    }
-                    return null;
-                  },
+        children: <Widget>[
+          Form(
+            key: _form,
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('lib/assets/foto_login.jpeg'),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Digite a senha',
-                      border: new OutlineInputBorder(
-                          borderRadius: const BorderRadius.all(
-                              const Radius.circular(20)))),
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  validator: (value) {
-                    if (value.trim().isEmpty) {
-                      return 'Insira uma senha para login';
-                    }
-                    return null;
-                  },
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Digite o Email',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira um email para login';
+                      } else if (!value.contains('@') ||
+                          !value.endsWith('.com')) {
+                        return 'Insira um email válido';
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.values.last,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(right: 90),
-                      child: RaisedButton(
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Digite a senha',
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20)))),
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    validator: (value) {
+                      if (value.trim().isEmpty) {
+                        return 'Insira uma senha para login';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RaisedButton(
                         child: Text(
                           'Cadastrar',
                           style: TextStyle(fontSize: 20),
@@ -83,27 +84,30 @@ class _LoginState extends State<Login> {
                         padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20)),
+                        onPressed: () => {
+                          Navigator.of(context)
+                              .pushNamed(AppRoutes.ESTABELECIMENTOS_FORMS)
+                        },
+                      ),
+                      RaisedButton(
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        textColor: Colors.white,
+                        color: Color(0xff1d3557),
+                        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
                         onPressed: () => verificarLogin(),
                       ),
-                    ),
-                    RaisedButton(
-                      child: Text(
-                        'Entrar',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      textColor: Colors.white,
-                      color: Color(0xff1d3557),
-                      padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      onPressed: () => verificarLogin(),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }

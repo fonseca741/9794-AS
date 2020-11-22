6d6b95c531a3b199789e26d770f957889fa1036a
Pedro Volpato <43215730+volpato98@users.noreply.github.com>
2020-11-22 19:46:53 -0300

    Adicionando as telas Chat/Acessibilidade

---
+++ lib/Views/ajuda_view.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class Ajuda extends StatelessWidget{
+ 
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Configurações',style: TextStyle(fontSize: 22),),
+        centerTitle: true,
+
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Center(
+            
+              child: Text('Acessibilidade', style: TextStyle(fontSize: 22) ,
+              ),
+            ),
+            
+          ),
+          Divider(
+                    color: Color(0xff1d3557),
+          ),
+          Padding(padding: const EdgeInsets.all(20) 
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40), 
+            child: Text('Para obermos mais acessibilidade à todos o sistema tem suporte para deficientes visuais!', style: TextStyle(fontSize: 20)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40), 
+            child: Text('O Sistema conta com narração automática da tela e auxílio na maioria das funções do aplicativo', style: TextStyle(fontSize: 20)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40), 
+            child: Text('Aperte o botão Abaixo para Iniciar O Processo', style: TextStyle(fontSize: 20)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: Icon(Icons.audiotrack,color: Colors.green[300], size: 50,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 70.0),
+              child: RaisedButton(
+                child: Text(
+                   'Começar a Utilizar o Processo',
+                   style: TextStyle(fontSize: 22),
+                    ),
+                    textColor: Colors.white,
+                    color: Color.fromRGBO(95, 140, 245, 1),
+                    padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                    onPressed: () => {
+                      Alert(
+                        context: context,
+                        type: AlertType.success,
+                        title: "Iniciou o Processo com Sucesso!",
+                        buttons: [
+                          DialogButton(
+                            child: Text(
+                              "OK",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                            width: 120,
+                          )
+                        ],
+                      ).show(),
+                    },
+               ),
+           ),
+
+          
+        ],
+        ),
+      )
+    );      
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:projeto1/Views/ajuda_view.dart';
+import 'package:projeto1/Views/chat_view.dart';
 import 'package:projeto1/Views/pedido_list.dart';
 import 'package:projeto1/provider/pedido.dart';
 import 'package:provider/provider.dart';
+
+import 'Routes/app_Routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,11 +26,15 @@ class MyApp extends StatelessWidget {
         title: 'iClothes',
         theme: ThemeData(
         
-         primarySwatch: Colors.blue,
+         primaryColor: Color(0xff1d3557),
         
           visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: PedidoLista(),
+      routes:{
+        AppRoutes.HOME:(_) => Ajuda(),
+
+      } ,
+      
     ),
     );
   }
